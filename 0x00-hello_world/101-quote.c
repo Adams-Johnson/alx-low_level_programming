62969bd31a8479ef5dfcc60bf0c7e0ff0aad66b7
“Adams-Johnson” <teejohnny52@gmail.com>
2023-10-05 17:13:39 -0700

    Hello World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,16 @@
 #!/bin/bash
-#include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 1 (Success)
-*/
-int main(void)
-{
-	write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
-}	
+int main(void) {
+    const char message[] = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
+    ssize_t len = sizeof(message) - 1;
+    ssize_t written = write(2, message, len);
+
+    if (written == len) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
