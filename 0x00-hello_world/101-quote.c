5fe35fe5f5a6d424ed22e638e194f5600cf4da16
“Adams-Johnson” <teejohnny52@gmail.com>
2023-10-05 17:24:36 -0700

    Hello World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,15 +5,10 @@
  *
  * Return: Always 0 (Success)
 */ 
-int main(void) {
-    const char message[] = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
-    ssize_t len = sizeof(message) - 1;
-    ssize_t written = write(2, message, len);
-
-    if (written == len) {
-        return 1;
-    } else {
-        return 0;
-    }
+int main(void) 
+{
+    char str[] = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
+    write(2, str, sizeof(str) - 1);
+    return (1);
 }
 
