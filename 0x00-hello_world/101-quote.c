743fd78a5014d70c2b51267ae9d34e629a2022b2
“Adams-Johnson” <teejohnny52@gmail.com>
2023-10-05 17:18:37 -0700

    Hello World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,10 @@
 #include <unistd.h>
 #include <string.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/ 
 int main(void) {
     const char message[] = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
     ssize_t len = sizeof(message) - 1;
