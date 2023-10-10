0b312fbfadce20d2d0b0bd8cd8c000d7f0d0568d
“Adams-Johnson” <teejohnny52@gmail.com>
2023-10-09 17:30:26 -0700

    Hello World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,16 @@
+#include <stdio.h>
 #include <unistd.h>
-#include <string.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
-*/ 
-int main(void) 
+* main - Entry point
+*
+* Return: Always 1 (Success)
+*/
+int main(void)
 {
-    char str[] = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
-    write(2, str, sizeof(str) - 1);
-    return (1);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
+
 
