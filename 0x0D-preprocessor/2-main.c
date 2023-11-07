0771f3c29f6a57d573bfcbbc67d5cb450e507883
“Adams-Johnson” <teejohnny52@gmail.com>
2023-11-07 13:04:40 -0800

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * This function serves as the entry point for your program.
+ * It calculates the sum of 98 and the SIZE macro.
+ *
+ * Return: Always o
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
